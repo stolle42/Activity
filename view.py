11f377d9faed8169c327a2f0a79a852b343d3c81
Stolle42 <simon.stoll@arcor.de>
2024-12-25 16:59:05 +0100

    explore some flask functionality

--- view.py
+++ view.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
+@app.route("/stats/<name>")
+def statistics_of(name):
+    return "Statistics of "+name
+
+@app.route("/stats")
+def statistics_general():
+    return "statistic page"
+
 @app.route("/")
 def mainapp():
-    return "hi"
+    return render_template("index.html")
 
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
